c0469e132c89114ea9f79504837b45492dea4b22
jeds2025 <jeds@outlook.com.br>
2025-06-15 12:09:50 -0300

    Atualizando o repositório casa 15/06/2025 às 12:00

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -8,3 +8,4 @@ def salario_bonus(bonus):
 
 
 salario_bonus(500)  # 2500
+print(salario)
--- 05 - Manipulação de arquivos/2_operacao_escrita.py
+++ 05 - Manipulação de arquivos/2_operacao_escrita.py
@@ -5,6 +5,6 @@ arquivo.writelines(["-"*36, "\n"])
 arquivo.write("Escrevendo dados em um novo arquivo.")
 arquivo.writelines(["\n", "-"*36, "\n"])
 
-arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto", "\n"])
+arquivo.writelines(["\n", "escrevendo novamente", "\n", "um", "\n", "novo", "\n", "texto simples", "\n"])
 arquivo.writelines(["\n", "-"*36, "\n"])
 arquivo.close()
